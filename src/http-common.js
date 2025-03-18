1310038d9960e21b60ae060c54b87a1758622b76
sean-kosc <sean.kamgaing@gmail.com>
2025-03-18 14:33:33 -0400

    FINAL VERSION

--- src/http-common.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-export default axios.create({
-  baseURL: 'http://localhost:8080/api',
-  withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json'
-  }
-})
